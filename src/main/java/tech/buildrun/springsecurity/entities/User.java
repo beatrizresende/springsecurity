3c4d390fc01df224f5d77ac7ddfd3aac9fe99423
beatriz <beatriz.resende@nectarcrm.com.br>
2025-05-21 17:31:20 -0300

    criando entidades e banco no docker

---
+++ src/main/java/tech/buildrun/springsecurity/entities/User.java
@@ -0,0 +1,62 @@
+package tech.buildrun.springsecurity.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(unique = true)
+    private String usename;
+
+    private String password;
+
+    //replica tudo que for feito na tabela de usuáriose e sempre traz essas infos (eager)
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsename() {
+        return usename;
+    }
+
+    public void setUsename(String usename) {
+        this.usename = usename;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
